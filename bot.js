0f9aef9fcc0e31c4b4435651a51ad944a589550b
revathiest <revathiest@gmail.com>
2019-01-30 17:29:07 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,8 +51,7 @@ if(!command.text.split(' ')[1]){
 }
   console.log('Count: ' + rollCount + ", Min: " + rollMin + ", Max: " + rollMax);
   relThis.res.writeHead(200);
-  postMessage(("@" + command.name + " rolled: " + roll(rollCount, rollMin, rollMax) + " " + " [" + rollCount + "-" + rollMax + "]"), command.name, command.user_id);
-
+  postMessage(("@" + command.name + " rolled: " + roll(rollCount, rollMin, rollMax) + " [" + rollMin + "-" + rollMax + "]"), command.name, command.user_id);
   relThis.res.end();
 }
 
