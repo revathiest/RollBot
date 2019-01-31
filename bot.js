e1d2d8607cb972f4c359030fcd44517c18ee49e5
revathiest <revathiest@gmail.com>
2019-01-30 19:18:36 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,14 +33,14 @@ if(!command.text.split(' ')[1]){
   postMessage("You have to tell me what to roll.", command.name, command.user_id);
   this.res.writeHead(200);
   this.res.end();
-} else if(command.text.split(' ')[1] && command.text.split(' ')[1].split('d')[1]){
-//dice setup 
-  rollCount = parseInt(command.text.split(' ')[1].split('d')[0]);
-  rollMax = parseInt(command.text.split(' ')[1].split('d')[1]);
 } else if(command.text.split(' ')[2]){
   postMessage("That doesnt make sense.", command.name, command.user_id);
   this.res.writeHead(200);
   this.res.end();
+} else if(command.text.split(' ')[1] && command.text.split(' ')[1].split('d')[1]){
+//dice setup 
+  rollCount = parseInt(command.text.split(' ')[1].split('d')[0]);
+  rollMax = parseInt(command.text.split(' ')[1].split('d')[1]);
 } else {
   rollCount = 1;
   rollMaxc = 0;
