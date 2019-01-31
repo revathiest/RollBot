c3185d47e7db82b09f5173f98f5fe163d63967e9
revathiest <revathiest@gmail.com>
2019-01-30 19:23:12 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,8 +31,8 @@ function commandHandler(relThis, command){
 if(!command.text.split(' ')[1]){
 //Need more info
   postMessage("You have to tell me what to roll.", command.name, command.user_id);
-//  this.res.writeHead(200);
-//  this.res.end();
+  relThis.res.writeHead(200);
+  relThis.res.end();
 } else if(command.text.split(' ')[2]){
   postMessage("That doesnt make sense.", command.name, command.user_id);
 //  this.res.writeHead(200);
