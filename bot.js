4ff6d231bcd2e5ee8e4db0488b3d870499b2d731
Ken Hart <revathiest@gmail.com>
2019-01-31 19:48:44 -0600

    code cleanup

--- bot.js
+++ bot.js
@@ -55,28 +55,35 @@ function commandHandler(relThis, command){
       rollString = rollString + ", " + rollTmp ;
     }
   }
+
   if(!rollCount == 0 && !rollMax == 0) {
+
     var rollTest = Math.ceil(rollSum / (rollCount * rollMax) * 100);
 	
     postMessage(("rolled: " + rollString + " [" + rollCount + "d" + rollMax + "] Total = " + rollSum), command.name, command.user_id);
 	
 	switch (true){
-	  case (rollSum == 1 && rollCount == 1 && rollMax == 20):
-	    postMessage(("I hope that wasnt a DC save.  Critical Failure!"), command.name, command.user_id);
-		break;
-	  case (rollTest == 100 && rollCount == 1 && rollMax == 20):
-	    postMessage(("Critical Roll!"), command.name, command.user_id);
-		break;
-	  case (rollTest < 20):
-	    postMessage(("That was an ugly roll..."), command.name, command.user_id);
+	  case (rollCount == 1 && rollMax == 20):
+	  	switch (true){
+	      case (rollSum == 1):
+	        postMessage(("I hope that wasnt a DC save.  Critical Failure!"), command.name, command.user_id);
+	        break;
+	      case (rollTest == 100):
+	        postMessage(("Critical Roll!"), command.name, command.user_id);
+	  	    break;
+          default:
+		}
 	    break;
-	  case (rollTest < 80):
-	    //nothing in here right now.  This is just a normal roll
-	    break;
-	  case (rollTest <= 100):
-	    postMessage(("Nice roll!"), command.name, command.user_id);
-		break;
-	  default:
+      case (rollTest < 20):
+        postMessage(("That was an ugly roll..."), command.name, command.user_id);
+        break;
+      case (rollTest < 80):
+        //nothing in here right now.  This is just a normal roll
+        break;
+      case (rollTest <= 100):
+        postMessage(("Nice roll!"), command.name, command.user_id);
+        break;
+      default:
 	}
     relThis.res.end();
   }
