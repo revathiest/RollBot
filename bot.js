d71365cae078ba1dadde03348e0eac830a120eb0
Ken Hart <revathiest@gmail.com>
2019-02-01 23:24:11 -0600

    Adding console debug messages

--- bot.js
+++ bot.js
@@ -26,13 +26,17 @@ function rollHandler(relThis, command){
   rollSum = 0,
   rollMod = 0;
   rollString = "";
-
+  console.log("Initiating dice roll");
+  relThis.res.writeHead(200);
+  relThis.res.end();
   if(!command.text.split(' ')[1]){
     postMessage("You have to tell me what to roll.", command.name, command.user_id);
+	console.log("No dice specified.");
     relThis.res.writeHead(200);
     relThis.res.end();
   } else if(command.text.split(' ')[2]){
     postMessage("That doesnt make sense.", command.name, command.user_id);
+	console.log("Invalid input.")
     relThis.res.writeHead(200);
     relThis.res.end();
   } else if(command.text.split(' ')[1] && command.text.split(' ')[1].split('d')[1]) {
@@ -42,14 +46,17 @@ function rollHandler(relThis, command){
 	if (command.text.split(' ')[1].split('+')[1]){
       rollMod = parseInt(command.text.split(' ')[1].split('+')[1]);
     }
+  console.log('Count: ' + rollCount + ", Max: " + rollMax + ", Modifier" + rollMod);
   } else {
     rollCount = 0;
     rollMax = 0;
   }
-  console.log('Count: ' + rollCount + ", Max: " + rollMax);
   relThis.res.writeHead(200);
 
   if ([4,6,8,10,12,20].indexOf(rollMax) > -1){
+  console.log("Rolling dice.")
+  relThis.res.writeHead(200);
+  relThis.res.end();
     for(i = 0; i < rollCount; i++){
     var rollTmp;
     rollTmp = roll(rollMax);
@@ -64,7 +71,6 @@ function rollHandler(relThis, command){
     if(!rollCount == 0 && !rollMax == 0) {
 
       var rollTest = Math.ceil(rollSum / (rollCount * rollMax) * 100);
-	
       postMessage(("rolled: " + rollString + " [" + rollCount + "d" + rollMax + "] Total = " + (rollSum + rollMod)), command.name, command.user_id);
 
 	  //Check for 1d20 Critical Rolls
@@ -97,6 +103,9 @@ function rollHandler(relThis, command){
     } 
   }else {
     postMessage("That is not a valid die type", command.name, command.user_id);
+	console.log("Invalid die type.");
+    relThis.res.writeHead(200);
+    relThis.res.end();
   }
 }
 
